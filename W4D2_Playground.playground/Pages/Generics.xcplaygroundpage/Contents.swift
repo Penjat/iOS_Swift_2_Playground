90c0bf082dd4675f5eb6fedcac1d06917f4b2318
Penjat <theSpencerSymington@Gmail.com>
2019-01-29 12:55:06 -0800

    done part 5 :qw :wq
    :
    :qw

--- W4D2_Playground.playground/Pages/Generics.xcplaygroundpage/Contents.swift
+++ W4D2_Playground.playground/Pages/Generics.xcplaygroundpage/Contents.swift
@@ -35,6 +35,10 @@ printMyTwoNumbers(num1: "three", num2: "four")
  */
 
 
+//func multiplyNumbers<K> (num1:K , num2: K) -> K{
+//  return num1 * num2
+//}
+
 /*:
  - Experiment:
  You might have run into error in the console regarding the multiplication operator can only be applied to numbered parameters. This makes sense as multiplying two Strings together doesn't make sense. So, we want to only take in variable types that *can* use the multiplication operator. In this case, we can limit the element type to types that conform to the `Numeric` protocol like below.
@@ -49,6 +53,9 @@ func multiply<Element: Numeric>(num1: Element, num2: Element) {
  - Experiment:
  Update your multiplication function and test it! Try using different variable types to see what works and what doesn't.
  */
+func multiplyNumbers<K: Numeric> (num1:K , num2: K) ->K{
+  return num1 * num2
+}
 
 
 /*:
@@ -59,9 +66,26 @@ func multiply<Element: Numeric>(num1: Element, num2: Element) {
  - Note:
  For this experiment, refrain from using the array method `indexOf`. Also the protocol `Equatable` might be useful here. Search it up to see what it's about.
  */
+func findInArray <Element: Equatable>(array: [Element], item: Element) -> Int{
+  //return -1 for not found
+  for i in 0..<array.count{
+    if(item == array[i]){
+      return i
+    }
+  }
+  
+  //if item not found
+  return -1
+  
+}
+let theIntArray = [4,6,22,93,1]
+findInArray(array: theIntArray, item: 93)
 
+findInArray(array: theIntArray, item: 0)
 
-
+let myFruitArray = ["pineapple","apple","banana"]
+findInArray(array: myFruitArray, item: "apple")
+findInArray(array: myFruitArray, item: "frogs")
 /*:
  - Callout(Challenge):
  During class you saw a simple implementation of a stack where data is inserted (pushed) to the top of the stack when data is added. When data is removed (pop) from the stack, it removes the first item at the top of the stack. We will now implement a similar data structure called a "queue" as a generic.
@@ -74,8 +98,42 @@ func multiply<Element: Numeric>(num1: Element, num2: Element) {
  - enqueue: add an item to the queue
  - dequeue: remove an item from the queue, and return the removed element
  */
+struct Person{
+  let name :String
+}
+var person1 = Person(name: "Joey")
+var person2 = Person(name: "Franatron")
+var person3 = Person(name: "The Big L")
+var person4 = Person(name: "Freshtastic")
+var person5 = Person(name: "M")
 
+struct Queue {
+  var lineup = [Person]()
+  mutating func getInLine(person: Person) {
+    lineup.append(person)
+    print("\(lineup)")
+  }
+  mutating func leaveLine(){
+    if lineup.count > 0{
+      lineup.remove(at: 0)
+      print("\(lineup)")
+    }else{
+      print("no one in line")
+    }
+  }
+}
 
-
+var theLineUp = Queue()
+theLineUp.getInLine(person: person1)
+theLineUp.getInLine(person: person2)
+theLineUp.getInLine(person: person3)
+theLineUp.leaveLine()
+theLineUp.getInLine(person: person4)
+theLineUp.getInLine(person: person5)
+theLineUp.leaveLine()
+theLineUp.leaveLine()
+theLineUp.leaveLine()
+theLineUp.leaveLine()
+theLineUp.leaveLine()
 //: [Next](@next)
 
